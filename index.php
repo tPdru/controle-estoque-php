61ad1e6aa4f6495ef99a6fb14b9d3f534d49ff8c
tPdru <pedroteixeiradeandrade@gmail.com>
2024-11-11 20:16:01 -0300

    projeto  contrle de estoque p2

--- index.php
+++ index.php
@@ -4,14 +4,47 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Home</title>
+	 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	 <link rel="stylesheet" type="text/css" href="css/estilo.css">
 </head>
 <body>
 
 <!--como fazer menu-->
-	<ul>
-		<li><a href=""></li>
+	<header>
+		<img src="imagens/logo.png" alt="logo">
+		<nav>
+			<ul>
+				<li><a href="#">Home</li>
+				<li><a href="#">Produtos</li>
+				<li><a href="#">Fornecedores</li>
+				<li><a href="#">ategorias</li>
+			</ul>
+		</nav>
+	</header>
+		<main class="container">
+		<h2>Conteúdos</h2>
+	</main>
+	<footer>
+		<h2>Rodapé</h2>
+	</footer>
 
-	</ul>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 </html>
